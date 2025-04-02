04be63570e922d030654486ca1eea25ad4fdafdd
Pyotr V. <ptrpl4@icloud.com>
2025-04-02 16:45:31 +0800

    add new folders and username to backup

--- new-backup.sh
+++ new-backup.sh
@@ -3,17 +3,24 @@
 
 # Create the backup directory if it doesn't exist
 current_date=$(date +%Y-%m-%d)
-backup_dir="${HOME}/dotfiles/backups/${current_date}"
+backup_dir="${HOME}/dotfiles/backups/${LOGNAME}/${current_date}"
 apps_backup_file="apps.txt"
+bin_backup_file="bin.txt"
+opt_backup_file="opt.txt"
+brew_backup_file="brew.txt"
 
 mkdir -p "${backup_dir}"
-echo "Backup dir created /dotfile/backups"
+echo "Backup dir created in /dotfile/backups"
 
 # copy list of installed apps
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # Running on macOS
     ls /Applications | cat > "${backup_dir}"/"${apps_backup_file}"
-    echo "Application list backed up"
+    ls /usr/local/bin | cat > "${backup_dir}"/"${bin_backup_file}"
+    ls /opt | cat > "${backup_dir}"/"${opt_backup_file}"
+    brew list --versions | cat > "${backup_dir}"/"${brew_backup_file}"
+
+    echo "Backup successful"
 else
     echo "Not running on macOS, skipping application backup"
 fi
