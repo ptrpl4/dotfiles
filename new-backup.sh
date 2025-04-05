a4f9b89900851c38e15149186ba440a4a12f540c
Pyotr Void <ptrpl4@icloud.com>
2025-04-05 22:57:09 +0800

    add linux support

--- new-backup.sh
+++ new-backup.sh
@@ -8,6 +8,7 @@ apps_backup_file="apps.txt"
 bin_backup_file="bin.txt"
 opt_backup_file="opt.txt"
 brew_backup_file="brew.txt"
+apt_backup_file="apt.txt"
 
 mkdir -p "${backup_dir}"
 echo "Backup dir created in /dotfile/backups"
@@ -21,6 +22,20 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     brew list --versions | cat > "${backup_dir}"/"${brew_backup_file}"
 
     echo "Backup successful"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Running on Linux
+    ls /usr/bin | cat > "${backup_dir}"/"${bin_backup_file}"
+    ls /opt | cat > "${backup_dir}"/"${opt_backup_file}"
+
+    # APT (Debian/Ubuntu)
+    if command -v apt &> /dev/null; then
+        dpkg --get-selections | cat > "${backup_dir}"/"${apt_backup_file}"
+        echo "APT packages backed up"
+    fi
+
+    echo "Linux backup successful"
 else
-    echo "Not running on macOS, skipping application backup"
+    echo "Unsupported operating system: $OSTYPE"
 fi
+
+# TODO add adguard, pihole
